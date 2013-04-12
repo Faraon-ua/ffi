f505d06e422bdf6bb7c8a4b0b0700cc320a19503
Oleksandr Dubrovsky <faraon.ua@gmail.com>
2013-04-12 19:40:53 +0300

    captify added

--- WestuaFFI/Internet/Scripts/site.js
+++ WestuaFFI/Internet/Scripts/site.js
@@ -21,4 +21,39 @@
         // caption span % of the image
         spanWidth: '100%'
     });
+
+   
+    
+    $('img.captioning').mouseenter(function () {
+        var caption;
+        if ($(this).parent().attr('class') != 'imgWrapper') {
+
+            var wrapper = $('<div>', {
+                class: 'imgWrapper',
+                style: 'overflow: hidden; padding: 0px; font-size: 0.1px; width: 160px; height: 160px; margin: 0px; border-style: none; border-width: 0px; border-color: rgb(51, 51, 51); position: relative;'
+            });
+
+            caption = $('<div>', {
+                class: 'imgCaption',
+                style: 'margin: 0; background-color: white; position: absolute; top:0; left: -160px; z-index: 2; border: 0px none; opacity: 1; width: 160px; height: 160px;',
+                html: $(this).attr("alt")
+            });
+
+            $(this).wrap(wrapper);
+            
+            wrapper = $(this).parent();
+            wrapper.append(caption).mouseleave(function() {
+                caption.animate({
+                    left: -160,
+                }, 200, function() {});
+    });
+            
+        } else {
+            caption = $(this).siblings("div.imgCaption");
+        }
+        
+        caption.animate({
+            left: 0,
+            }, 200, function() {});
+    });
 });
